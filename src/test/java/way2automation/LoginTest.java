430db41d6d49e31da3cb6a42e2ac4b07c4f20752
siddharth <ssiddharth387@gmail.com>
2019-12-26 11:07:09 +0530

    Initial commit

---
+++ src/test/java/way2automation/LoginTest.java
@@ -0,0 +1,39 @@
+package way2automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setup()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin()
+	{
+		driver.get("http://gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("ssiddharth387@gmail.com");
+		//driver.findElement(By.id("Passwd")).sendKeys("8871869733");
+	}	
+	
+	@AfterSuite
+	public void teardown()
+	{
+		driver.quit();
+	}
+}
